2e3a4c221c04893d86662ba31e10257a06b3b377
yutiansut <yutiansut@qq.com>
2019-04-20 02:19:33 +0800

    1.0 version

--- QUANTAXIS_RandomPrice/__init__.py
+++ QUANTAXIS_RandomPrice/__init__.py
@@ -1,9 +1,11 @@
 #
 import pandas as pd
-
 import numpy as np
 import matplotlib.pyplot as plt
 import click
+import random
+import copy
+
 
 class OrnsteinUhlenbeckActionNoise:
 
@@ -31,25 +33,54 @@ class OrnsteinUhlenbeckActionNoise:
         return 'OrnsteinUhlenbeckActionNoise(mu={}, sigma={})'.format(self.mu, self.sigma)
 
 
-def get_random_price(price, code='rb1905', tradingDay='20181119', mu=0, sigma=0.2, theta=0.15, dt=1e-2):
+time_index = pd.timedelta_range('21:00:00.500000', '23:00:00', freq='500ms').tolist() +\
+    pd.timedelta_range('09:00:00.500000', '10:15:00', freq='500ms').tolist() +\
+    pd.timedelta_range('10:30:00.500000', '11:30:00', freq='500ms').tolist() +\
+    pd.timedelta_range('13:30:00.500000', '15:00:00', freq='500ms').tolist()
+
+
+def get_random_price(price, code='rb1905', tradingDay='20181119', mu=0, sigma=0.2, theta=0.15, dt=1e-2, ifprint=False):
     ou_noise = OrnsteinUhlenbeckActionNoise(mu=np.array(mu))
-    plt.figure('data')
-    y = []
-    data = pd.read_csv('tick_temple.csv')
 
-    for _, item in data.iterrows():
-        item['InstrumentID'] = str(code)
-        item['LastPrice'] = (ou_noise()+1)*0.2*price + 0.8*price
+    data = []
+    tick_pickle = {
+        'InstrumentID': str(code),
+        'UpdateTime': '',
+        'UpdateMillisec': '',
+        'LastPrice': '',
+        'Volume': 0,
+        'TradingDay': tradingDay
+    }
+    for item in time_index:
+        x = str(item).split()[2]
+        if '.' in x:
+            tick_pickle['UpdateMillisec'] = 500
+        else:
+            tick_pickle['UpdateMillisec'] = 0
 
-    return data
+        tick_pickle['UpdateTime'] = x.split('.')[0]
+        tick_pickle['Volume'] += random.randint(50, 5000)
+        tick_pickle['LastPrice'] = (ou_noise()+1)*0.2*price + 0.8*price
+        data.append(copy.deepcopy(tick_pickle))
+        if ifprint:
+            print(tick_pickle)
+    return pd.DataFrame(data)
 
 
 @click.command()
-@click.option('--price',default=3600)
+@click.option('--price', default=3600)
 @click.option('--code', default='rb1905')
-def generate(price, code):
-    print(get_random_price(price, code))
-
+@click.option('--tradingday', default='20181119')
+@click.option('--mu', default=0)
+@click.option('--sigma', default=0.2)
+@click.option('--theta', default=0.15)
+@click.option('--dt', default=1e-2)
+@click.option('--ifprint', default=True)
+def generate(price, code, tradingday, mu, sigma, theta, dt, ifprint):
+    data = get_random_price(price, code, tradingday, mu, sigma, theta, dt, ifprint)
+    print(data)
+    data.LastPrice.plot()
+    plt.show()
 
 if __name__ == '__main__':
     print(get_random_price(3600))
